eed95187069abaf9627b2550fee8c243f0c973a0
Tanguy-Pellerin <50319760+Tanguy-Pellerin@users.noreply.github.com>
2020-12-16 00:22:09 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -7,62 +7,15 @@ class FlaskTests(unittest.TestCase):
 	def test_a_index(self):
 		responce = requests.get('http://0.0.0.0:8050/')
 		self.assertEqual(responce.status_code, 200)
-"""
-	def setUp(self):
-		os.environ['NO_PROXY'] = '0.0.0.0'
-		self.user = {
-			'uid': 314,
-			'fname': "Joerge",
-			"lname": "Carmen",
-			"credit": 30
-		}
-		pass
 		
-	def tearDown(self):
-		pass
+	def test_b_get_tweet(self):
+		parameters = {
+			'sentence': 'The media and establishment want me out of the race so badly -  I WILL NEVER DROP OUT OF THE RACE, WILL NEVER LET MY SUPPORTERS DOWN! #MAGA',
+			'form_type': 'get_tweets'
+		}
+		response = requests.post('http://0.0.0.0:8050/', data=parameters)
+		self.assertEqual(responce.status_code, 200)
 
-	
-	def test_a_index(self):
-		responce = requests.get('http://localhost:5000')
-		self.assertEqual(responce.status_code, 200)
-		
-	def test_b_add_user(self):
-		
-		params = {
-			'uid': self.user['uid'],
-			'fname': self.user['fname'],
-			"lname": self.user['lname'],
-			"form_type": "add_user"
-		}
-		responce = requests.post('http://localhost:5000', data=params)
-		self.assertEqual(responce.status_code, 200)
-		#self.assertEqual(responce.content, 'success'.encode())
-		
-	def test_c_add_credit(self):
-		
-		params = {
-			'uid': self.user['uid'],
-			'credit': self.user['credit'],
-			"form_type": "add_credit"
-		}
-		responce = requests.post('http://localhost:5000', data=params)
-		self.assertEqual(responce.status_code, 200)
-		#self.assertEqual(responce.content, 'success'.encode())
-	
-	def test_d_view_user(self):
-		params = {
-			'uid': self.user['uid'],
-			"form_type": "view_user"
-		}
-		expected = "first name: {}, last name: {}, credit: {}".format(
-																self.user['fname'],
-																self.user['lname'],
-																float(self.user['credit'])
-																)
-		responce = requests.post('http://localhost:5000', data=params)
-		self.assertEqual(responce.status_code, 200)
-		#self.assertEqual(responce.content, expected.encode())
-"""
 if __name__ == '__main__':
 	unittest.main()		
 
