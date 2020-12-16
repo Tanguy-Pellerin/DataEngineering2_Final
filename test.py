66ecadb08c6978435a9fd05ca51a058e5c229ed0
TanguyPellerin <tanguy@perreaux.fr>
2020-12-16 01:04:03 +0100

    Adding test

---
+++ test.py
@@ -0,0 +1,13 @@
+from locust import HttpUser, task, between
+
+class WebsiteTestUser(HttpUser):
+	
+    @task(1)
+    def hello_world(self):
+        self.client.get("/")
+"""
+    def get_tweet(self):
+    	self.client.post("http://0.0.0.0:5000/form_type", json={
+    		'sentence': 'The media and establishment want me out of the race so badly -  I WILL NEVER DROP OUT OF THE RACE, WILL NEVER LET MY SUPPORTERS DOWN! #MAGA',
+    		})
+"""
