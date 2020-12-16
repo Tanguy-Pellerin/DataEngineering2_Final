dbf86124637dab402f39edd15e8afcca9b1dc74b
Tanguy-Pellerin <50319760+Tanguy-Pellerin@users.noreply.github.com>
2020-12-16 10:45:39 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ class WebsiteTestUser(HttpUser):
 
 @events.quitting.add_listener
 def _(environment, **kw):
-    if environment.stats.total.total_rps > 17:
-        logging.error("Test failed due to >1000 requests per minute")
+    if environment.stats.total.total_rps < 17:
+        logging.error("Test failed due to less than 1000 requests per minute")
         environment.process_exit_code = 1
     else:
         environment.process_exit_code = 0
